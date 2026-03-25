a0990af45ea3cd81b62a9e4f60b09b70adab4c44
YL - Jun Senido <plukyljunsenido@gmail.com>
2026-03-25 10:21:45 +0800

    Changing logo

--- app/Http/Controllers/EmployeeRequestController.php
+++ app/Http/Controllers/EmployeeRequestController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Kreait\Firebase\Factory;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 class EmployeeRequestController extends Controller
 {
